b74ac89622636986ae877d896e64e05c26e6e317
ZhongRuge <635246203@qq.com>
2025-07-10 16:53:19 +0800

    添加iddr工程，输入框架完成，需要测试输入数据阶段

---
+++ iddr_test/iddr_test.srcs/sim_1/new/iddrtest_tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+
+module iddrtest_tb();
+
+    reg clk_100m;
+    reg rst_n;
+    reg [7:0] single_data_input;
+
+    iddrtest iddrtest_inst (
+        .clk_100m(clk_100m),
+        .rst_n(rst_n),
+        .single_data_input(single_data_input)
+    );
+
+    initial clk_100m = 1;
+    always #5 clk_100m = ~clk_100m;
+    reg [7:0] inputdata [0:4095];
+
+    integer i;
+    initial begin
+        single_data_input = 0;
+        $readmemh("E:/zrg/FPGA/Projects/LearningProjects/iddr_test/sine_wave.txt", inputdata);
+        rst_n = 0;
+        #200;
+        rst_n = 1;
+        
+        #200;      
+        
+        for (i = 0; i < 4096; i = i + 1) begin
+            @(posedge clk_100m);
+            single_data_input = inputdata[i];
+        end
+
+        #40000;
+        $stop;
+        
+    end
+
+endmodule
---
+++ iddr_test/iddr_test.srcs/sources_1/new/iddrtest.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+
+module iddrtest(
+    input clk,
+    input rst_n,
+    input [7:0] single_data_input
+    );
+
+
+
+    reg [7:0] whole_data_input [0:4095];
+
+    integer reset_i = 0;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            for (reset_i = 0; reset_i < 4096; reset_i = reset_i + 1) begin
+                whole_data_input[reset_i] <= 0;
+            end
+        end
+        else begin
+            whole_data_input[addra] <= single_data_input;
+        end
+    end
+
+
+    blk_mem_gen_0 input_data_bram (
+        .clka(clka),    // input wire clka
+        .wea(wea),      // input wire [0 : 0] wea
+        .addra(addra),  // input wire [11 : 0] addra
+        .dina(dina),    // input wire [7 : 0] dina
+        .clkb(clkb),    // input wire clkb
+        .addrb(addrb),  // input wire [11 : 0] addrb
+        .doutb(doutb)  // output wire [7 : 0] doutb
+    );
+
+endmodule
