63901dae043e585968c23ec0eca2c1d50f8aa3c7
Zhongruge <635246203@qq.com>
2025-10-19 21:28:21 +0800

    添加IIC，UART_RAM_TFT工程

---
+++ UART_RAM_TFT/UART_RAM_TFT.srcs/sources_1/new/img_rx_wr.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module img_rx_wr(
+    input clk,
+    input rst_n,
+
+    input [7:0] rx_data,
+    input rx_done,
+
+    output ram_wr_en,
+    output [15:0] ram_wr_addr,
+    output [15:0] ram_wr_data
+    );
+
+    
+
+endmodule
